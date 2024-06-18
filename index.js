616c54e4994da2f5de7f7db4e9fe329c1cc0c4c8
Jaime gallego <104177104+JaimeG3@users.noreply.github.com>
2024-06-18 18:34:06 -0500

    navigate

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { sequelize } = require("./src/db");
 const server = require("./src/server");
 
+
 server.listen(process.env.PORT || 3000, () => {
     console.log(`Server listening on port ${process.env.PORT || 3000}`);
   });
--- src/controllers/indexControllers.js
+++ src/controllers/indexControllers.js
@@ -1,1 +1,16 @@
-console.log("hola mundo de sam");
+const puppeteer = require( "puppeteer")
+
+const navigateWebPage = async ( ) =>{
+    const browser = await puppeteer.launch({
+        headless : false,
+        slowMo: 400
+    })
+    const page = await browser.newPage()
+
+    await page.goto("https://quotes.toscrape.com")
+    await page.click(`a[href="/login"]`)
+    await new Promise(r = setTimeout(r, 500))
+    await browser.close()
+}
+
+module.exports = navigateWebPage
