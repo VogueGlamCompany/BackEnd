22b25c2c045fe04b2bcb232ead8f7b83df5475ac
Jaime gallego <104177104+JaimeG3@users.noreply.github.com>
2024-06-18 19:02:55 -0500

    beta

--- src/controllers/indexControllers.js
+++ src/controllers/indexControllers.js
@@ -1,4 +1,5 @@
 const puppeteer = require( "puppeteer")
+const fs = require ("fs")
 
 const navigateWebPage = async ( ) =>{
     const browser = await puppeteer.launch({
@@ -13,4 +14,57 @@ const navigateWebPage = async ( ) =>{
     await browser.close()
 }
 
-module.exports = navigateWebPage
+const getDataWebPage = async ( ) =>{
+    const browser = await puppeteer.launch({
+        headless : false,
+        slowMo: 400
+    })
+    const page = await browser.newPage()
+
+    await page.goto("https://www.example.com")
+    await page.click(`a[href="/login"]`)
+    const result = await page.evaluate(()=> {
+       const title =  document.querySelector("h1").innerText 
+       const description = document.querySelector("p").innerText 
+       const more = document.querySelector("a").innerText 
+       return{
+        title,
+        description,
+        more
+       }
+    })
+    
+    await browser.close()
+}
+
+const handleDynamicWebPage = async ( ) =>{
+    const browser = await puppeteer.launch({
+        headless : false,
+        slowMo: 400
+    })
+    const page = await browser.newPage()
+
+    await page.goto("https://quotes.toscrape.com")
+    await page.click(`a[href="/login"]`)
+    const result = await page.evaluate(()=> {
+       const quotes =  document.querySelectorAll(".quote")
+       const data = [...quotes].map(quote => {
+        const quouteText = quote.querySelector(".text").innerText;
+        const author = quote.querySelector(".author").innerText;
+        const tags = [...quote.document.querySelector(".tag")].map((tag)=> tag.innerText)
+        return{
+            quouteText,
+            author,
+            tags
+           }
+       })
+       return data
+    })
+    console.log(result)
+
+    fs.writeFile("quotes.json", JSON.stringify(result,null,2))
+    
+    await browser.close()
+}
+
+module.exports = navigateWebPage,getDataWebPage,handleDynamicWebPage
